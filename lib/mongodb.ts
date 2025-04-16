6f6d3f21662a7ebf9ef8398eb20d849c983f5a32
nicharr-nn <nicha.rua@gmail.com>
2025-04-16 18:49:50 +0700

    moved get and save

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+import ParkingSpotRecord from "@/lib/models/ParkingSpot";
 
 export default class MongoConnectSingleton {
   private static instance: MongoConnectSingleton;
@@ -36,5 +38,58 @@ export default class MongoConnectSingleton {
       throw new Error("MongoDB connection error");
     }
   }
+  static async getParkingSpots(): Promise<NextResponse> {
+    try {
+      const mongoConnect = MongoConnectSingleton.getInstance();
+      await mongoConnect.connectToDB();
+
+      const records = await ParkingSpotRecord.findOne();
+
+      if (!records) {
+        throw new Error("No records found");
+      }
+
+      return NextResponse.json({
+        message: "Fetched parking spots successfully",
+        data: records,
+      });
+    } catch (error) {
+      console.error("Error saving parking spots:", error);
+      return NextResponse.json({ error: "Failed to save" }, { status: 500 });
+    }
+  }
+
+  static async saveParkingSpots(req: Request): Promise<NextResponse> {
+    try {
+      const mongoConnect = MongoConnectSingleton.getInstance();
+      await mongoConnect.connectToDB();
+
+      const body = await req.json();
+      const { parkingSpots } = body;
+
+      const existing = await ParkingSpotRecord.findOne();
+
+      if (existing) {
+        existing.parkingSpots = parkingSpots;
+        await existing.save();
+
+        return NextResponse.json({
+          message: "Updated existing record",
+          id: existing._id,
+        });
+      } else {
+        const record = await ParkingSpotRecord.create({
+          parkingSpots,
+        });
+
+        return NextResponse.json({
+          message: "Saved new record",
+          id: record._id,
+        });
+      }
+    } catch (error) {
+      console.error("Error saving parking spots:", error);
+      return NextResponse.json({ error: "Failed to save" }, { status: 500 });
+    }
+  }
 }
- 
